77de85020caf13333772919ec5a5d740b4d79649
kmnczr <minczerkristof@gmail.com>
2023-09-27 13:25:45 +0200

    list the user accommodations

--- backend/index.js
+++ backend/index.js
@@ -106,19 +106,53 @@ app.post('/register', async (req,res) => {
 
   app.post('/places', (req,res) => {
     const {token} = req.cookies;
-  const {
-    title,address,addedPhotos,description,price,
-    perks,extraInfo,checkIn,checkOut,maxGuests,
-  } = req.body;
-  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-    if (err) throw err;
+    const {
+      title,address,addedPhotos,description,price,
+      perks,extraInfo,checkIn,checkOut,maxGuests,
+    } = req.body;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
     const placeDoc = await Place.create({
-      owner:userData.id,price,
+      owner:userData.id,
       title,address,photos:addedPhotos,description,
       perks,extraInfo,checkIn,checkOut,maxGuests,
     });
     res.json(placeDoc);
   });
+  })
+
+  app.get('/places', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      const {id} = userData
+      res.json( await Place.find({owner:id}) )
+    })
+  })
+
+  app.put('/places', async (req,res) => {
+    const {token} = req.cookies;
+    const {
+      id, title,address,addedPhotos,description,
+      perks,extraInfo,checkIn,checkOut,maxGuests,price,
+    } = req.body;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const placeDoc = await Place.findById(id);
+      if (userData.id === placeDoc.owner.toString()) {
+        placeDoc.set({
+          title,address,photos:addedPhotos,description,
+          perks,extraInfo,checkIn,checkOut,maxGuests,price,
+        });
+        await placeDoc.save();
+        res.json('ok');
+      }
+    });
+  });
+  
+
+  app.get('/places/:id', async (req,res) => {
+    const {id} = req.params
+    res.json(await Place.findById(id))
   })
 
 
