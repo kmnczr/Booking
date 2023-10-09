685dfe290926c2cdb69aaae816131c0e89e9fad5
kmnczr <minczerkristof@gmail.com>
2023-10-09 11:16:34 +0200

    display places on index page

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,9 +1,29 @@
+import {useEffect, useState} from "react"
+import axios from "axios"
 
 export default function IndexPage() {
-    
-    return(
-      <div>
-       index page here
-      </div>
-    )
+  const [places,setPlaces] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/places').then(response => {setPlaces(response.data)})
+  }, [])
+
+  return(
+    <div className="mt-8 grid gap-x-6 gap-y-8 grid-col-2 md:grid-cols-3 lg:grid-cols-4">
+      {places.length > 0 && places.map(place =>(
+        <div>
+          <div className="bg-gray-500 mb-2 rounded-2xl flex">
+            {place.photos?.[0] && (
+              <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/'+place.photos?.[0]} alt=""/>
+            )}
+          </div>
+          <h2 className="font-bold leading-4">{place.address}</h2>
+          <h3 className="text-sm leading-4 text-gray-500">{place.title}</h3>
+          <div className="mt-2">
+            <span className="font-bold">${place.price}</span> per night
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
