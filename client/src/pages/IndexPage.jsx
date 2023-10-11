35c09bb41781f226f64902c55d3ef35c7dd0f1bd
kmnczr <minczerkristof@gmail.com>
2023-10-11 10:56:41 +0200

    make listed places links to its own profile page

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom";
 import {useEffect, useState} from "react"
 import axios from "axios"
 
@@ -11,7 +12,7 @@ export default function IndexPage() {
   return(
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-col-2 md:grid-cols-3 lg:grid-cols-4">
       {places.length > 0 && places.map(place =>(
-        <div>
+        <Link to={'/place/'+place._id}>
           <div className="bg-gray-500 mb-2 rounded-2xl flex">
             {place.photos?.[0] && (
               <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/'+place.photos?.[0]} alt=""/>
@@ -22,7 +23,7 @@ export default function IndexPage() {
           <div className="mt-2">
             <span className="font-bold">${place.price}</span> per night
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   )
